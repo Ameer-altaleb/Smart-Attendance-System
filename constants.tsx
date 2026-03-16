e53f81869ba99260472fcd42e4123bfe82392a8f
Ameer-altaleb <aaltaleb@reliefexperts.org>
2026-03-16 06:25:03 +0300

    Update APP_VERSION to 2.3.0-pwa

--- constants.tsx
+++ constants.tsx
@@ -12,7 +12,7 @@ export const INITIAL_HOLIDAYS: Holiday[] = [];
 export const INITIAL_NOTIFICATIONS: Notification[] = [];
 
 export const INITIAL_PROJECTS: Project[] = [];
-export const APP_VERSION = '2.2.0-pwa';
+export const APP_VERSION = '2.3.0-pwa';
 
 export const INITIAL_TEMPLATES: MessageTemplate[] = [
   { id: 't1', type: 'check_in', content: 'تم تسجيل دخولك بنجاح في الوقت المحدد. نتمنى لك يوماً سعيداً!' },
